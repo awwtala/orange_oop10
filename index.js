501bf7009f51fa5d8091d3be33b574f16d38f8f0
Tala Awwad <awwadtala08@gmail.com>
2024-10-29 14:19:19 -0700

    update to the lib folder

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
 const inquirer = require("inquirer");
+
+const SVG = require("./lib/svg");
+const Text = require("./lib/text");
+const { Shape, Circle, Square, Triangle } = require("./lib/shapes");
 const fs = require("fs");
-const {circle, triangle, square};
-const shapes = require("./lib/shapes");
 
-inquirer.prompt([
-  {
-    name: "text",
-    type: "input",
-    message: "Enter your logo text (up to 3 characters or numbers)",
-  },
-  {
-    name: "textColor",
-    type: "input",
-    message: "Pick your text color (Color keyword)",
-  },
-  {
-    name: "shape",
-    type: "Pick your shape:",
-    message: ["Circle", "Triangle", "Square"],
-  },
-  {
-    name: "shapeColor",
-    type: "input",
-    message: "Pick your shape color (Color keyword)",
-  },
-])
+inquirer
+  .prompt([
+    {
+      name: "text",
+      type: "input",
+      message: "Enter your logo text (up to 3 characters or numbers)",
+    },
+    {
+      name: "textColor",
+      type: "input",
+      message: "Pick your text color (Color keyword)",
+    },
+    {
+      name: "shape",
+      type: "list",
+      message: "Pick your shape:",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      name: "shapeColor",
+      type: "input",
+      message: "Pick your shape color (Color keyword)",
+    },
+  ])
+  .then((answers) => {
+    // console.log(answers);
 
-  .then((response) => {
-    console.log(
-      "Your logo will be ",
-      response.text,
-      response.textColor,
-      response.shape,
-      response.shapeColor,
-      "!"
-    );
-    makingLogo(response);
+    const myText = new Text(answers.text, answers.textColor);
+    const textElement = myText.render();
+
+    let myShape;
+
+    if (answers.shape == "Circle") {
+      myShape = new Circle(answers.shapeColor);
+    } else if (answers.shape == "Square") {
+      myShape = new Square(answers.shapeColor);
+    } else if (answers.shape == "Triangle") {
+      myShape = new Triangle(answers.shapeColor);
+    }
+
+    const shapeElement = myShape.render();
+
+    const mySVG = new SVG(shapeElement, textElement);
+    const svgCode = mySVG.render();
+
+    // console.log(svgCode);
+
+    fs.writeFile("./examples/output.svg", svgCode, () => {
+      console.log("Logo has been generated successfully!");
+    });
   });
-  function makingLogo(response) {
-    if (response.shape = circle){
-    shape = new circle (response.shapeColor)
-    }
-    else if (response.shape = square){
-    shape = new square (response.shapeColor)
-    }
-    else if (response.shape = triangle){
-    shape = new triangle (response.shapeColor)}
-const SVG {
-}
-  }
